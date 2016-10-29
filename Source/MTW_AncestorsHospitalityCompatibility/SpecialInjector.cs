40f64c1260084c44e79b5a4adcaa1eedac64a09d
MoyTW <moytravis@gmail.com>
2016-10-29 15:52:37 -0700

    Add Detour code
    Was copied from Ancestors, which was from Hospitality. Eurgh. Hope the CCL
    breakout goes well and updates quick enough for me to be comfortable with
    using it as a dep.

---
+++ Source/MTW_AncestorsHospitalityCompatibility/SpecialInjector.cs
@@ -0,0 +1,34 @@
+﻿using Verse;
+using System.Linq;
+using System.Reflection;
+
+namespace MTW_AncestorsHospitalityCompatibility
+{
+    // Copied from Hospitality - MTW
+    public class SpecialInjector
+    {
+        public virtual bool Inject()
+        {
+            Log.Error("This should never be called.");
+            return false;
+        }
+    }
+
+    [StaticConstructorOnStartup]
+    internal static class DetourInjector
+    {
+        private static Assembly Assembly { get { return Assembly.GetAssembly(typeof(DetourInjector)); } }
+        private static string AssemblyName { get { return Assembly.FullName.Split(',').First(); } }
+        static DetourInjector()
+        {
+            LongEventHandler.QueueLongEvent(Inject, "Initializing", true, null);
+        }
+
+        private static void Inject()
+        {
+            var injector = new SpecialInjectorWorker();
+            if (injector.Inject()) Log.Message(AssemblyName + " injected.");
+            else Log.Error(AssemblyName + " failed to get injected properly.");
+        }
+    }
+}
